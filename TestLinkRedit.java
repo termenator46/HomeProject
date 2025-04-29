f9733e397bd4f602ea2d2a21947330592025d989
termenator46 <mitcel258@gmail.com>
2025-04-29 21:37:02 +0200

    Add files via upload

---
+++ TestLinkRedit.java
@@ -0,0 +1,26 @@
+package test;
+
+import com.Pages.HomePage;
+import com.Pages.RedirectLink;
+import com.drmoqa.core.TestBase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestLinkRedit extends TestBase {
+    RedirectLink redirectLink;
+    HomePage homePage;
+    @BeforeEach
+    public void precondition(){
+        redirectLink =new RedirectLink(driver);
+        homePage = new HomePage(driver);
+
+    }
+    @Test
+    @DisplayName("verfi all ELemets")
+    public void VerfiStatusCodes(){
+        homePage.ClickRedirectLink();
+        redirectLink.triggerredirect();
+        redirectLink.verifyElements();
+    }
+}
